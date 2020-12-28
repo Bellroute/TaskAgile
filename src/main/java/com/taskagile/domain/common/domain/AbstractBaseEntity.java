abbd756aef884ddef3e5db2204d01928f8c6e340
Bellroute <kjkun7631@gmail.com>
2020-12-28 18:10:19 +0900

    implement registration in domain model and repository

---
+++ src/main/java/com/taskagile/domain/common/domain/AbstractBaseEntity.java
@@ -0,0 +1,14 @@
+package com.taskagile.domain.common.domain;
+
+import java.io.Serializable;
+
+public abstract class AbstractBaseEntity implements Serializable {
+
+  private static final long serialVersionUID = -1153931912966528994L;
+
+  public abstract boolean equals(Object obj);
+
+  public abstract int hashCode();
+
+  public abstract String toString();
+}
