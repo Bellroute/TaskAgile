8e1e6550776c3712bb4b7b58132503aebbe39268
Bellroute <kjkun7631@gmail.com>
2021-01-07 22:53:43 +0900

    implement login page front-end

--- front-end/src/views/RegisterPage.vue
+++ front-end/src/views/RegisterPage.vue
@@ -1,11 +1,8 @@
 <template>
-  <div class="container">
+  <div class="public">
     <div class="row justify-content-center">
-      <div class="register-form">
-        <div class="logo-wrapper">
-          <img class="logo" src="/static/images/logo.png" />
-          <div class="tagline">Open source task management tool</div>
-        </div>
+      <div class="form">
+        <Logo/>
         <form @submit.prevent="submitForm">
           <div v-show="errorMessage" class="alert alert-danger failed">{{ errorMessage }}</div>
           <div class="form-group">
@@ -14,8 +11,12 @@
             <div class="field-error" v-if="$v.form.username.$dirty">
               <div class="error" v-if="!$v.form.username.required">Username is required</div>
               <div class="error" v-if="!$v.form.username.alphaNum">Username can only contain letters and numbers</div>
-              <div class="error" v-if="!$v.form.username.minLength">Username must have at least {{$v.form.username.$params.minLength.min}} letters.</div>
-              <div class="error" v-if="!$v.form.username.maxLength">Username is too long. It can contains maximium {{$v.form.username.$params.maxLength.max}} letters.</div>
+              <div class="error" v-if="!$v.form.username.minLength">Username must have at least
+                {{$v.form.username.$params.minLength.min}} letters.
+              </div>
+              <div class="error" v-if="!$v.form.username.maxLength">Username is too long. It can contains maximium
+                {{$v.form.username.$params.maxLength.max}} letters.
+              </div>
             </div>
           </div>
           <div class="form-group">
@@ -24,7 +25,9 @@
             <div class="field-error" v-if="$v.form.emailAddress.$dirty">
               <div class="error" v-if="!$v.form.emailAddress.required">Email address is required</div>
               <div class="error" v-if="!$v.form.emailAddress.email">This is not a valid email address</div>
-              <div class="error" v-if="!$v.form.emailAddress.maxLength">Email address is too long. It can contains maximium {{$v.form.emailAddress.$params.maxLength.max}} letters.</div>
+              <div class="error" v-if="!$v.form.emailAddress.maxLength">Email address is too long. It can contains
+                maximium {{$v.form.emailAddress.$params.maxLength.max}} letters.
+              </div>
             </div>
           </div>
           <div class="form-group">
@@ -32,8 +35,12 @@
             <input type="password" class="form-control" id="password" v-model="form.password">
             <div class="field-error" v-if="$v.form.password.$dirty">
               <div class="error" v-if="!$v.form.password.required">Password is required</div>
-              <div class="error" v-if="!$v.form.password.minLength">Password is too short. It can contains at least {{$v.form.password.$params.minLength.min}} letters.</div>
-              <div class="error" v-if="!$v.form.password.maxLength">Password is too long. It can contains maximium {{$v.form.password.$params.maxLength.max}} letters.</div>
+              <div class="error" v-if="!$v.form.password.minLength">Password is too short. It can contains at least
+                {{$v.form.password.$params.minLength.min}} letters.
+              </div>
+              <div class="error" v-if="!$v.form.password.maxLength">Password is too long. It can contains maximium
+                {{$v.form.password.$params.maxLength.max}} letters.
+              </div>
             </div>
           </div>
           <button type="submit" class="btn btn-primary btn-block">
@@ -49,30 +56,22 @@
         </form>
       </div>
     </div>
-    <footer class="footer">
-      <span class="copyright">&copy; 2020 TaskAgile.com</span>
-      <ul class="footer-links list-inline float-right">
-        <li class="list-inline-item"><a href="#">About</a></li>
-        <li class="list-inline-item"><a href="#">Terms of Service</a></li>
-        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-        <li class="list-inline-item">
-          <a
-            href="https://github.com/taskagile/vuejs.spring-boot.mysql"
-            target="_blank"
-            >GitHub</a
-          >
-        </li>
-      </ul>
-    </footer>
+   <PageFooter/>
   </div>
 </template>
 
 <script>
   import registrationService from '@/services/registration'
-  import { required, email, minLength, maxLength, alphaNum } from 'vuelidate/lib/validators'
+  import {alphaNum, email, maxLength, minLength, required} from 'vuelidate/lib/validators'
+  import Logo from "@/components/Logo";
+  import PageFooter from "@/components/PageFooter.Vue";
 
   export default {
     name: 'RegisterPage',
+    components: {
+      Logo,
+      PageFooter
+      },
     data: function () {
       return {
         form: {
@@ -104,7 +103,7 @@
       }
     },
     methods: {
-      submitForm () {
+      submitForm() {
         this.$v.$touch()
         if (this.$v.$invalid) {
           return
@@ -122,59 +121,7 @@
 </script>
 
 <style lang="scss" scoped>
-.container {
-  max-width: 900px;
-}
-.register-form {
-  margin-top: 50px;
-  max-width: 320px;
-}
-.logo-wrapper {
-  text-align: center;
-  margin-bottom: 40px;
-  .tagline {
-    line-height: 180%;
-    color: #666;
-  }
-  .logo {
-    max-width: 150px;
-    margin: 0 auto;
-  }
-}
-.register-form {
-  .form-group label {
-    font-weight: bold;
-    color: #555;
-  }
-  .accept-terms {
-    margin: 20px 0 40px 0;
-  }
-}
-.footer {
-  width: 100%;
-  font-size: 13px;
-  color: #666;
-  line-height: 40px;
-  border-top: 1px solid #ddd;
-  margin-top: 50px;
-  .list-inline-item {
-    margin-right: 10px;
-  }
-  a {
-    color: #666;
-  }
-}
-.form-group {
-  .error {
-    line-height: 1;
-    display: none;
-    margin-top: 5px;
-  }
-}
-.field-error {
-  .error {
-    display: block;
-    color: #ff0000;
-  }
-}
+    .accept-terms {
+      margin: 20px 0 40px 0;
+    }
 </style>
--- front-end/tests/unit/LoginPage.spec.js
+++ front-end/tests/unit/LoginPage.spec.js
@@ -1,10 +1,148 @@
-import Vue from "vue";
+import {createLocalVue, mount} from '@vue/test-utils'
+import VueRouter from 'vue-router'
 import LoginPage from "@/views/LoginPage";
+import Vuelidate from 'vuelidate'
+import authenticationService from '@/services/authentication'
+
+// vm.$router에 접근할 수 있도록
+// 테스트에 Vue Router 추가하기
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+localVue.use(Vuelidate)
+
+const router = new VueRouter()
+
+// authenticationService의 mock
+jest.mock('@/services/authentication')
 
 describe("LoginPage.vue", () => {
-  it("should render correct contents", () => {
-    const Constructor = Vue.extend(LoginPage);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector("h1").textContent).toEqual("TaskAgile");
-  });
+  let wrapper
+  let fieldUsername
+  let fieldPassword
+  let buttonSubmit
+  let authenticateSpy
+
+  beforeEach(() => {
+    wrapper = mount(LoginPage, {
+      localVue,
+      router
+    })
+
+    fieldUsername = wrapper.find('#username')
+    fieldPassword = wrapper.find('#password')
+    buttonSubmit = wrapper.find('form button[type="submit"]')
+
+    authenticateSpy = jest.spyOn(authenticationService, 'authenticate')
+  })
+
+  afterEach(() => {
+    authenticateSpy.mockReset()
+    authenticateSpy.mockRestore()
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('should render login form', () => {
+    expect(wrapper.find('.logo').attributes().src)
+      .toEqual('/static/images/logo.png')
+
+    expect(wrapper.find('.tagline').text())
+      .toEqual('Open source task management tool')
+
+    expect(fieldUsername.element.value).toEqual('')
+
+    expect(fieldPassword.element.value).toEqual('')
+
+    expect(buttonSubmit.text()).toEqual('Sign in')
+
+    expect(wrapper.find('.link-sign-up').attributes().href)
+      .toEqual('/register')
+
+    expect(wrapper.find('.link-forgot-password'))
+      .toBeTruthy()
+  })
+
+  it('should contain data model with initial values', () => {
+    expect(wrapper.vm.form.username).toEqual('')
+    expect(wrapper.vm.form.password).toEqual('')
+  })
+
+  it('should have form inputs bound with data model', () => {
+    const username = 'sunny'
+    const password = 'VueJsRocks!'
+
+    new Promise(() => {
+      wrapper.vm.form.username = username
+      wrapper.vm.form.password = password
+    }).then(() => {
+      expect(fieldUsername.element.value).toEqual(username)
+      expect(fieldPassword.element.value).toEqual(password)
+    })
+  })
+
+  it('should have form submit event handler `submitForm`', () => {
+    const stub = jest.fn()
+
+    wrapper.setMethods({submitForm: stub})
+    buttonSubmit.trigger('submit')
+
+    expect(stub).toBeCalled()
+  })
+
+  it('should succeed when credentials are valid', async () => {
+    expect.assertions(2)
+
+    const stub = jest.fn()
+    wrapper.vm.$router.push = stub
+
+    wrapper.vm.form.username = 'sunny'
+    wrapper.vm.form.password = 'JestRocks!'
+
+    wrapper.vm.submitForm()
+    expect(authenticateSpy).toBeCalled()
+
+    await wrapper.vm.$nextTick()
+    expect(stub).toHaveBeenCalledWith({name: 'HomePage'})
+  })
+
+  it('should fail when credentials are invalid', async () => {
+    expect.assertions(2)
+
+    // In the mock, only password `JestRocks!` combined with
+    // username `sunny` or `sunny@taskagile.com` is valid
+    wrapper.vm.form.username = 'sunny'
+    wrapper.vm.form.password = 'MyPassword!'
+    expect(wrapper.find('.failed').isVisible()).toBe(false)
+
+    wrapper.vm.submitForm()
+    expect(authenticateSpy).toBeCalled()
+
+    await wrapper.vm.$nextTick(null, () => {
+      expect(wrapper.find('.failed').isVisible()).toBe(true)
+    })
+  })
+
+  it('should have validation to prevent invalid data submit', () => {
+    // Empty form
+    wrapper.vm.submitForm()
+    expect(authenticateSpy).not.toHaveBeenCalled()
+
+    // Only username is valid
+    wrapper.vm.form.username = 'sunny'
+    wrapper.vm.submitForm()
+    expect(authenticateSpy).not.toHaveBeenCalled()
+
+    // Only email is valid
+    wrapper.vm.form.username = 'sunny@taskagile.com'
+    wrapper.vm.submitForm()
+    expect(authenticateSpy).not.toHaveBeenCalled()
+
+    // Only password is valid
+    wrapper.vm.form.password = 'MyPassword!'
+    wrapper.vm.form.username = ''
+    wrapper.vm.submitForm()
+    expect(authenticateSpy).not.toHaveBeenCalled()
+  })
 });
